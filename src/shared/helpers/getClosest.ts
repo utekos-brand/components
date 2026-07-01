2caccaca84cd1cb349f15808a02768e4b631a630
utekos-brand <system@comfyrobe.no>
2026-07-01 23:20:02 +0200

    Add getClosestParent function for DOM traversal
    Implement getClosestParent function to find the closest matching parent element based on a selector.

---
+++ src/shared/helpers/getClosest.ts
@@ -0,0 +1,47 @@
+import { warn } from '../helpers'
+
+export type TargetSelector = string
+export type RootElement = Element
+
+/**
+ * getClosestParent returns the first element that matches the `selector` starting with the given `element` and traversing up through its parent.
+ *
+ * @param  {string} selector CSS class or tag name (tag name with upper case chars, like "BUTTON")
+ * @param  {Element} element The element to begin with
+ * @param  {boolean} excludeSelf Whether to exclude the given element itself from the search. Default: `false`.
+ * @return {Element} Found element or `null`
+ */
+export const getClosestParent = (
+  selector: TargetSelector,
+  element: RootElement,
+  excludeSelf = false
+): Element | null => {
+  try {
+    const tagName = /[A-Z]/.test(selector) ? selector : null
+    const className = tagName ? null : selector.replace(/^\./, '')
+
+    const contains = (element: Element) => {
+      if (!element) {
+        return null
+      }
+
+      if (tagName) {
+        return element?.tagName === tagName
+      }
+
+      return element?.classList.contains(className)
+    }
+
+    if (excludeSelf) {
+      element = element?.parentElement
+    }
+
+    while (element && !contains(element)) {
+      element = element?.parentElement
+    }
+  } catch (e) {
+    warn('getClosest: Failed to traverse DOM tree:', e)
+  }
+
+  return element
+}
